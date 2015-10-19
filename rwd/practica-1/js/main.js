aa8fd4ca2d5b83b128270e8274fddb6d2740e33f
Diartch25 <diartch25@gmail.com>
2015-10-19 11:48:57 -0600

    Se colocó el menu

---
+++ rwd/practica-1/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready( function() {
+    var displayClass = $('.nav-bar').css('display');
+    console.log(displayClass);
+    $('#display').click(
+        function(){
+            if(displayClass == "none"){
+                $('.nav-bar').slideToggle().css('display', 'block');
+            }else if (displayClass == "block") {
+                $('.nav-bar').slideToggle().css('display', 'none');
+            }
+    })
+    
+} );
